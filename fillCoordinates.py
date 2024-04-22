7bca9915321a46b9ad332e194d419bc338c9d3a3
Sandun Viduranga <sandunviduranga97@outlook.com>
2024-04-22 16:14:27 +0530

    refine

--- fillCoordinates.py
+++ fillCoordinates.py
@@ -72,6 +72,43 @@ def fill_coordinates(contour_array):
 
 
 
+def measure_distance_old(longest_contour,second_longest_contour,frame_contours):
+    tolerance=0.15
+    thickness=32
+    defective = False
+    for ref_point in range(0, int(len(second_longest_contour)/2), 100):
+        min_dist=np.linalg.norm(longest_contour[ref_point][0] - second_longest_contour[ref_point][0])
+        x_outer,y_outer = longest_contour[ref_point][0]
+        x_inner,y_inner = second_longest_contour[ref_point][0]
+        for coordinates in second_longest_contour:
+            dist = np.linalg.norm(longest_contour[ref_point][0] - coordinates[0])
+            if dist<min_dist:
+                min_dist=dist
+                x_inner,y_inner = coordinates[0]
+        #print(min_dist)
+        #print(str(x_inner)+","+str(y_inner))
+        #print(str(x_outer)+","+str(y_outer))
+        if ((min_dist>thickness*(1+tolerance)) or (min_dist<thickness*(1-tolerance)) ) :
+            defective = True
+            color=(0,0,255)
+        else:
+            color=(0,0,0)
+        cv.line(frame_contours,(x_outer,y_outer),(x_inner,y_inner),color,2)
+        cv.putText(frame_contours,("d="+str(min_dist)) , (x_outer,y_outer), cv.FONT_HERSHEY_PLAIN, 1.5 , color, 2, cv.LINE_AA)
+        
+    if(defective):
+        cv.putText(frame_contours,("Defective") , (400,500), cv.FONT_HERSHEY_PLAIN, 2 , (0, 0, 255), 2, cv.LINE_AA)
+        cv.putText(frame_contours,("Balance Out") , (400,550), cv.FONT_HERSHEY_PLAIN, 2 , (0, 0, 255), 2, cv.LINE_AA)
+    else:
+        cv.putText(frame_contours,("Non-Defective") , (400,500), cv.FONT_HERSHEY_PLAIN, 2 , (0, 255, 0), 2, cv.LINE_AA)
+    
+    cv.putText(frame_contours,("Thickness : "+ str(thickness)) , (400,575), cv.FONT_HERSHEY_PLAIN, 1.5 , (255, 255, 255), 2, cv.LINE_AA)
+    cv.putText(frame_contours,("Tolerance : "+str(tolerance)+"%") , (400,600), cv.FONT_HERSHEY_PLAIN, 1.5 , (255, 255, 255), 2, cv.LINE_AA)
+    return
+
+
+
+
 def measure_distance(longest_contour,second_longest_contour,frame_contours):
     tolerance=0.15
     thickness=32
