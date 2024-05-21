b592a8c282ebfa37a796dba6e5e7a520cf2e90c6
Sandun Viduranga <sandunviduranga97@outlook.com>
2024-05-21 13:10:13 +0530

    Otsu thresholding activated

--- edgeDetect .py
+++ edgeDetect .py
@@ -4,17 +4,31 @@ import os
 from fillCoordinates import fill_coordinates
 from fillCoordinates import measure_distance
 from fillCoordinates import display
+import time
 
 #camera= cv.VideoCapture(0)
 
 # Check if the file exists
-file_path = 'gusset.jpg'
+file_path = 'gusset (5).jpg'
 if not os.path.exists(file_path):
     print("Error: File '{}' not found.".format(file_path))
     exit()
 
 # Read the image
 original_frame = cv.imread(file_path)
+original_frame = cv.resize(original_frame, (960, 1280))
+original_frame_resized = cv.resize(original_frame, (960, 1280))
+grayscale_image = cv.cvtColor(original_frame, cv.COLOR_BGR2GRAY)
+
+# Apply Gaussian Blur
+blurred_image = cv.GaussianBlur(grayscale_image, (5, 5), 0)
+
+# Otsu's Binarization
+_, otsu_thresholded = cv.threshold(blurred_image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+otsu_resized = cv.resize(otsu_thresholded, (960, 1280))       
+cv.imshow('otsu_thresholded', otsu_resized)
+
 threshold1=200
 threshold2=300
 while True:
@@ -22,14 +36,11 @@ while True:
     #_,original_frame=camera.read()
     #cv.imshow('Original Image', original_frame)
 
-    # Apply Laplacian edge detection
-    laplace = cv.Laplacian(original_frame, cv.CV_64F)
-    laplace = np.uint8(np.absolute(laplace))
-    #cv.imshow('Laplacian Edge', laplace)
 
     # Apply Canny edge detection
-    canny = cv.Canny(original_frame, threshold1, threshold2)
-    #cv.imshow('Canny Edge', canny)
+    canny = cv.Canny(otsu_thresholded, threshold1, threshold2)
+    canny_resized = cv.resize(canny, (960, 1280))
+    cv.imshow('Canny Edge', canny_resized)
 
     # Find contours
     contours, _ = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
@@ -39,15 +50,40 @@ while True:
     second_max_length = 0
     longest_contour = None
     second_longest_contour = None
+    relative_length0 = 0
+    relative_length1 = 0
+
+    #start_time = time.time()
 
     for contour in contours:
         length = cv.arcLength(contour, closed=True)
+        print(str(length))
         if length > max_length:
+            if max_length != 0:
+                relative_length1=(length-max_length)/max_length
+                if 0.05 < relative_length1 < 0.5:
+                    second_max_length = max_length
+                    second_longest_contour = longest_contour
+                    #print("longest : "+str(max_length)+" second longest : "+str(second_max_length)+ " relative : "+str(relative_length1))
+            else:
+                second_max_length = max_length
+                second_longest_contour = longest_contour
             max_length = length  
             longest_contour = contour
+
         elif length > second_max_length:
-            second_max_length = length
-            second_longest_contour = contour
+            if second_max_length != 0:
+                relative_length1=(max_length-length)/length
+                if 0.05 < relative_length1 < 0.5:
+                    second_max_length = length
+                    second_longest_contour = contour
+                    #print("longest : "+str(max_length)+" second longest : "+str(second_max_length)+ " relative : "+str(relative_length1))
+    print("longest : "+str(max_length)+" second longest : "+str(second_max_length)+ " relative : "+str(relative_length1))                
+
+
+    #end_time = time.time()
+
+    #print("Execution time = "+str(1000*(end_time-start_time))+"ms")
         
     # Highlight the longest edge
     frame_contours = original_frame.copy()
@@ -81,8 +117,13 @@ while True:
         
         measure_distance(longest_contour,second_longest_contour,frame_contours)
         #display(frame_contours,longest_contour)
-        cv.imshow('Edges', frame_contours) 
+        
+        frame_contours_resized = cv.resize(frame_contours, (960, 1280))
+        cv.imshow('Edges', frame_contours_resized) 
+        
         cv.imwrite("Output.jpg",frame_contours)
+        cv.imwrite("otsu_thresholded.jpg",otsu_thresholded)
+        cv.imwrite("canny.jpg",canny)
     else:
         cv.imshow('Edges', original_frame)
         print("Invalid contours")
--- fillCoordinates.py
+++ fillCoordinates.py
@@ -99,50 +99,6 @@ def fill_coordinates(contour_array):
     return new_array
 
 
-
-def measure_distance_(longest_contour,second_longest_contour,frame_contours):
-    tolerance=0.15
-    thickness=32
-    defective = False
-    count=0
-    #print(len(second_longest_contour))
-    for ref_point in range(0, int((len(second_longest_contour)-1)), 100):
-        #print(ref_point)
-        min_dist=np.linalg.norm(longest_contour[ref_point][0] - second_longest_contour[ref_point][0])
-        x_outer,y_outer = longest_contour[ref_point][0]
-        x_inner,y_inner = second_longest_contour[ref_point][0]
-        for coordinates in second_longest_contour:
-            dist = np.linalg.norm(longest_contour[ref_point][0] - coordinates[0])
-            if dist<min_dist:
-                min_dist=dist
-                x_inner,y_inner = coordinates[0]
-        #print(min_dist)
-        #print(str(x_inner)+","+str(y_inner))
-        #print(str(x_outer)+","+str(y_outer))
-        if ((min_dist>thickness*(1+tolerance)) or (min_dist<thickness*(1-tolerance)) ) :
-            defective = True
-            color=(0,0,255)
-        else:
-            color=(0,0,0)
-        cv.line(frame_contours,(x_outer,y_outer),(x_inner,y_inner),color,2)
-        cv.putText(frame_contours,(str(ref_point)+": d="+str(min_dist)) , (x_outer,y_outer), cv.FONT_HERSHEY_PLAIN, 1.5 , color, 2, cv.LINE_AA)
-        count=count+1
-        
-    if(defective):
-        cv.putText(frame_contours,("Defective") , (400,500), cv.FONT_HERSHEY_PLAIN, 2 , (0, 0, 255), 2, cv.LINE_AA)
-        cv.putText(frame_contours,("Balance Out") , (400,550), cv.FONT_HERSHEY_PLAIN, 2 , (0, 0, 255), 2, cv.LINE_AA)
-    else:
-        cv.putText(frame_contours,("Non-Defective") , (400,500), cv.FONT_HERSHEY_PLAIN, 2 , (0, 255, 0), 2, cv.LINE_AA)
-    
-    cv.putText(frame_contours,("Thickness : "+ str(thickness)) , (400,575), cv.FONT_HERSHEY_PLAIN, 1.5 , (255, 255, 255), 2, cv.LINE_AA)
-    cv.putText(frame_contours,("Tolerance : "+str(tolerance)+"%") , (400,600), cv.FONT_HERSHEY_PLAIN, 1.5 , (255, 255, 255), 2, cv.LINE_AA)
-    return
-
-
-
-
-
-
 def measure_distance(longest_contour,second_longest_contour,frame_contours):
     tolerance=0.20
     thickness=0
@@ -198,7 +154,8 @@ def measure_distance(longest_contour,second_longest_contour,frame_contours):
         ref_index=min_index
         itr_count=itr_count+1
         sum=sum+min_dist
-        if itr_count>50:
+        gap =50
+        if itr_count>gap:
             avg=sum/itr_count            
             sum=0
             itr_count=0
@@ -209,7 +166,7 @@ def measure_distance(longest_contour,second_longest_contour,frame_contours):
                 color=(0,0,0)                   
             cv.putText(frame_contours,str(round(avg,2)) , (x_out_display,y_out_display), cv.FONT_HERSHEY_PLAIN, 2 , color, 2, cv.LINE_AA)
             cv.line(frame_contours,(x_out_display,y_out_display),(x_in_display,y_in_display),color,2)
-        elif itr_count==25:
+        elif itr_count==(gap/2):
             x_out_display,y_out_display = (longest_contour[min_index][0])
             x_in_display,y_in_display = (inner_coordinates[0])
 
@@ -232,3 +189,41 @@ def display(frame,cont) :
         x,y=cont[i][0]
         cv.putText(frame,str(i),(x,y), cv.FONT_HERSHEY_PLAIN, 1.5 , (0, 0, 0), 2, cv.LINE_AA)
     
+
+def measure_distance_(longest_contour,second_longest_contour,frame_contours):
+    tolerance=0.5
+    thickness=80
+    defective = False
+    count=0
+    #print(len(second_longest_contour))
+    for ref_point in range(0, int((len(second_longest_contour)-1)), 100):
+        #print(ref_point)
+        min_dist=np.linalg.norm(longest_contour[ref_point][0] - second_longest_contour[ref_point][0])
+        x_outer,y_outer = longest_contour[ref_point][0]
+        x_inner,y_inner = second_longest_contour[ref_point][0]
+        for coordinates in second_longest_contour:
+            dist = np.linalg.norm(longest_contour[ref_point][0] - coordinates[0])
+            if dist<min_dist:
+                min_dist=dist
+                x_inner,y_inner = coordinates[0]
+        #print(min_dist)
+        #print(str(x_inner)+","+str(y_inner))
+        #print(str(x_outer)+","+str(y_outer))
+        if ((min_dist>thickness*(1+tolerance)) or (min_dist<thickness*(1-tolerance)) ) :
+            defective = True
+            color=(0,0,255)
+        else:
+            color=(0,0,0)
+        cv.line(frame_contours,(x_outer,y_outer),(x_inner,y_inner),color,2)
+        cv.putText(frame_contours,(str(ref_point)+": d="+str(min_dist)) , (x_outer,y_outer), cv.FONT_HERSHEY_PLAIN, 1.5 , color, 2, cv.LINE_AA)
+        count=count+1
+        
+    if(defective):
+        cv.putText(frame_contours,("Defective") , (400,500), cv.FONT_HERSHEY_PLAIN, 2 , (0, 0, 255), 2, cv.LINE_AA)
+        cv.putText(frame_contours,("Balance Out") , (400,550), cv.FONT_HERSHEY_PLAIN, 2 , (0, 0, 255), 2, cv.LINE_AA)
+    else:
+        cv.putText(frame_contours,("Non-Defective") , (400,500), cv.FONT_HERSHEY_PLAIN, 2 , (0, 255, 0), 2, cv.LINE_AA)
+    
+    cv.putText(frame_contours,("Thickness : "+ str(thickness)) , (400,575), cv.FONT_HERSHEY_PLAIN, 1.5 , (255, 255, 255), 2, cv.LINE_AA)
+    cv.putText(frame_contours,("Tolerance : "+str(tolerance)+"%") , (400,600), cv.FONT_HERSHEY_PLAIN, 1.5 , (255, 255, 255), 2, cv.LINE_AA)
+    return
