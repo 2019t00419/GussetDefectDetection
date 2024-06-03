4063b5107d5889c985ea67c1f7bd7d8819759f8e
Sandun Viduranga <sandunviduranga97@outlook.com>
2024-06-03 07:51:49 +0530

    yolo with webcam

--- YOLO/yolo.py
+++ YOLO/yolo.py
@@ -1,35 +1,58 @@
-import cv2 as cv
 
-def identify_edges(contours):
-    
-    max_length = 0
-    second_max_length = 0
-    longest_contour = None
-    second_longest_contour = None
-    relative_length0 = 0
-    relative_length1 = 0
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import time
+ 
+cap = cv2.VideoCapture(0)  # For Webcam
+cap.set(3, 1280)
+cap.set(4, 720)
 
-    for contour in contours:
-            length = cv.arcLength(contour, closed=True)
-            #print(str(length))
-            if length > max_length:
-                if max_length != 0:
-                    relative_length1=(length-max_length)/max_length
-                    if 0.05 < relative_length1 < 0.5:
-                        second_max_length = max_length
-                        second_longest_contour = longest_contour
-                        #print("longest : "+str(max_length)+" second longest : "+str(second_max_length)+ " relative : "+str(relative_length1))
-                else:
-                    second_max_length = max_length
-                    second_longest_contour = longest_contour
-                max_length = length  
-                longest_contour = contour
-
-            elif length > second_max_length:
-                if second_max_length != 0:
-                    relative_length1=(max_length-length)/length
-                    if 0.05 < relative_length1 < 0.5:
-                        second_max_length = length
-                        second_longest_contour = contour
-                     
-    return(longest_contour,second_longest_contour)  
+# cap = cv2.VideoCapture("../Videos/motorbikes.mp4")  # For Video
+ 
+ 
+#image_path = "YOLO/images/in/gusset (1).jpg"
+model = YOLO('../weights/yolov8n.pt')
+ 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+ 
+prev_frame_time = 0
+new_frame_time = 0
+ 
+while True:
+    new_frame_time = time.time()
+    success, img = cap.read()
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+ 
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+ 
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+    print(fps)
+ 
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
